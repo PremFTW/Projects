4d80ecfcd39a990edd2a90ca646e1d36021f645c
PremFTW <premtech16@gmail.com>
2021-11-16 10:50:15 -0500

    Moved Lab1 Q2 and Q3 to Lab1 folder

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -3,12 +3,26 @@
 
 int main()
 {
-	int a1 = 6, a2 = 8, a3 = 14, a4 = 41, a5 = 44;
+	int w = 3, y = 4, z = 7, b;
+	double x = -1.3, angle = 60, pi = 3.14, radAngle, a, c, d, e;
 
-	float avg = (a1 + a2 + a3 + a4 + a5) / 5.0;
+	// Problem 1 Number 1
+	a = fabs(x);
 
-	printf("Ages: \n");
-	printf("Age1 %d -- Age2 %d -- Age3 %d -- Age4 %d -- Age5 %d -- Average Age %f", a1, a2, a3, a4, a5, avg);
+	// Problem 1 Number 2
+	b = pow(w, y);
+
+	// Problem 1 Number 3
+	radAngle = (angle * pi) / 180.0;
+	c = pow(pow(cos(radAngle), 2.0), 1.0 / 3.0);
+
+	// Problem 1 Number 4
+	d = fabs(exp(-z));
+
+	//// Problem 1 Number 5
+	e = log10(z);
+
+	printf("Number 1 = %f \n Number 2 = %d \n Number 3 = %f \n Number 4 = %f \n Number 5 = %f", a, b, c, d, e);
 
 	return 0;
 }
