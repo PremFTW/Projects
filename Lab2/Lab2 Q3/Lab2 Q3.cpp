9607191163f43c1337c55da42144f459b56c145d
PremFTW <premtech16@gmail.com>
2021-11-16 10:55:33 -0500

    Updated Lab2
    Lab 2 now has projects for Questions 2 and 3.

---
+++ Lab2/Lab2 Q3/Lab2 Q3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<math.h>
+
+int main()
+{
+	int a1 = 6, a2 = 8, a3 = 14, a4 = 41, a5 = 44;
+
+	float avg = (a1 + a2 + a3 + a4 + a5) / 5.0;
+
+	printf("Ages: \n");
+	printf("Age1 %d -- Age2 %d -- Age3 %d -- Age4 %d -- Age5 %d -- Average Age %f", a1, a2, a3, a4, a5, avg);
+
+	return 0;
+}
